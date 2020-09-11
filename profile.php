6184ae9ae5d5e0dcf08027c610e5e35070605030
Tanjil Hasan <41756334+tanjilh136@users.noreply.github.com>
2020-09-11 19:11:35 +0600

    Update profile.php

--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
 /**
  * Header File of Template
  * Created by PhpStorm.
- * User: Tushar Khan
+ * User: Tanjil Hasan
  * Date: 9/8/2017
  * Time: 6:11 AM
  */
@@ -22,6 +22,7 @@ $categoryObject = new Category();
 $userObject     = new User();
 $cartObject     = new Cart();
 $messageObject  = new Message();
+$allCategoryObject  = new AllCategory();
 ?>
 <?php
 $formateObject = new Formate();
@@ -36,7 +37,7 @@ global $cat;
     <!-- for-mobile-apps -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="author" content="Tushar Khan" />
+    <meta name="author" content="Tanjil Hasan" />
     <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
         function hideURLbar(){ window.scrollTo(0,1); } </script>
     <!-- //for-mobile-apps -->
