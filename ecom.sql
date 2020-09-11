ef0a20d43a041db9e27766f6d4c4a93cee282884
Tanjil Hasan <41756334+tanjilh136@users.noreply.github.com>
2020-09-11 18:44:16 +0600

    Update ecom.sql

--- ecom.sql
+++ ecom.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 28, 2017 at 08:05 PM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.8
+-- Generation Time: Apr 11, 2020 at 12:55 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -34,15 +33,17 @@ CREATE TABLE `ecom_admin` (
   `adminlastname` varchar(20) NOT NULL,
   `adminemail` varchar(200) NOT NULL,
   `level` varchar(100) NOT NULL,
-  `adminpass` varchar(250) NOT NULL
+  `adminpass` varchar(250) NOT NULL,
+  `address` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `ecom_admin`
 --
 
-INSERT INTO `ecom_admin` (`id`, `adminfastname`, `adminlastname`, `adminemail`, `level`, `adminpass`) VALUES
-(1, 'Tushar', 'Khan', 'tushar@gmail.com', 'uploads/c5494226b1.jpg', '827ccb0eea8a706c4c34a16891f84e7b');
+INSERT INTO `ecom_admin` (`id`, `adminfastname`, `adminlastname`, `adminemail`, `level`, `adminpass`, `address`) VALUES
+(1, 'Tanjil', 'hasan', 'tanjilh136@gmail.com', 'uploads/70f6f1bec7.jpg', '827ccb0eea8a706c4c34a16891f84e7b', ''),
+(2, 'Tanjil', 'Hasan', 'tanjilh136@gmail.com', 'uploads/70f6f1bec7.jpg', '27d421e4938e934fd3e0f1620e4b5919', 'sreepur,savar');
 
 -- --------------------------------------------------------
 
@@ -63,8 +64,8 @@ CREATE TABLE `ecom_admin_message` (
 --
 
 INSERT INTO `ecom_admin_message` (`id`, `email`, `subject`, `message`, `date`) VALUES
-(1, 'tushar.khan0122@gmail.com', 'Test Sunject', 'Message', '2017-09-21'),
-(2, 'tushar.khan0122@gmail.com', 'Test Subject', 'Message', '2017-09-21');
+(1, 'tanjilh136@gmail.com', 'Test Sunject', 'Message', '2017-09-21'),
+(2, 'tanjilh136@gmail.com', 'Test Subject', 'Message', '2017-09-21');
 
 -- --------------------------------------------------------
 
@@ -154,8 +155,9 @@ CREATE TABLE `ecom_customer` (
 --
 
 INSERT INTO `ecom_customer` (`userid`, `name`, `email`, `image`, `pass`, `date`, `zip`, `country`, `city`, `phone`) VALUES
-(1, 'Tushar Khan', 'tushar.khan0122@gmail.com', 'uploads/22b842dc92.jpg', '827ccb0eea8a706c4c34a16891f84e7b', '2017-09-17', '1230', 'Bangladesh', 'Dhaka', '+8801962837564'),
-(2, 'Tushar Khan', 'tushar.khan0122@gmail.com', 'uploads/22b842dc92.jpg', 'b59c67bf196a4758191e42f76670ceba', '2017-09-17', '1230', 'Bangladesh', 'Dhaka', '+8801962837564');
+(1, 'Tanjil Hasan', 'tanjilh136@gmail.com', 'uploads/372ef6a306.jpg', '827ccb0eea8a706c4c34a16891f84e7b', '2017-09-17', '1349', 'Bangladesh', 'Dhaka', '+8801701046785'),
+(2, 'Tanjil Hasan', 'tanjilh136@gmail.com', 'uploads/22b842dc92.jpg', 'b59c67bf196a4758191e42f76670ceba', '2017-09-17', '1230', 'Bangladesh', 'Dhaka', '+8801962837564'),
+(3, 'Md Rakibul Hasan Ant', 'mdanto345@gmail.com', 'NULL', '0803e2e4e36693ebeea4d5c935b695d5', '2020-04-11', '', '', '', '+8801849624222');
 
 -- --------------------------------------------------------
 
@@ -177,8 +179,8 @@ CREATE TABLE `ecom_customer_message` (
 --
 
 INSERT INTO `ecom_customer_message` (`messageid`, `userid`, `subject`, `message`, `messagedate`, `useremail`) VALUES
-(1, 1, 'Message Subject', 'Mesage Body', '2017-09-21', 'tushar.khan0122@gmail.com'),
-(2, 2, 'Another Message Subject', 'Another Mesage Body', '2017-09-21', 'tushar.khan0122@gmail.com');
+(1, 1, 'Message Subject', 'Mesage Body', '2017-09-21', 'tanjilh136@gmail.com'),
+(2, 2, 'Another Message Subject', 'Another Mesage Body', '2017-09-21', 'tanjilh136@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -193,7 +195,7 @@ CREATE TABLE `ecom_customer_order` (
   `price` float NOT NULL,
   `quantity` int(11) NOT NULL,
   `productname` varchar(50) NOT NULL,
-  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
   `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -202,9 +204,9 @@ CREATE TABLE `ecom_customer_order` (
 --
 
 INSERT INTO `ecom_customer_order` (`id`, `customerid`, `productid`, `price`, `quantity`, `productname`, `date`, `status`) VALUES
-(6, 1, 6, 245, 1, 'Mobile Phone 3', '2017-09-21 20:25:23', 0),
+(6, 1, 6, 245, 1, 'Mobile Phone 3', '2017-09-21 20:25:23', 1),
 (7, 1, 14, 680, 1, 'LED Tv', '2017-09-21 20:25:23', 1),
-(8, 1, 7, 850, 1, 'Laptop', '2017-09-21 20:28:40', 0);
+(8, 1, 7, 850, 1, 'Laptop', '2017-09-21 20:28:40', 1);
 
 -- --------------------------------------------------------
 
@@ -229,21 +231,21 @@ CREATE TABLE `ecom_product` (
 --
 
 INSERT INTO `ecom_product` (`proid`, `proname`, `catid`, `brandid`, `body`, `price`, `image`, `type`, `date`) VALUES
-(1, 'Speakers', 2, 2, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 320, 'uploads/1cbd8190ab.jpg', 'Black', '2017-09-12'),
-(2, 'Headphones', 2, 1, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.', 150, 'uploads/8c1bbc01ca.jpg', 'Sliver', '2017-09-12'),
-(3, 'Audio Player', 2, 3, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.', 180, 'uploads/1911fee744.jpg', 'Blue', '2017-09-12'),
-(4, 'Mobile Phone1', 1, 1, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.', 350, 'uploads/25bdaf15a9.jpg', 'Black', '2017-09-12'),
-(5, 'Mobile Phone 2', 1, 2, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.', 302, 'uploads/5829e9c083.jpg', 'Sliver', '2017-09-12'),
-(6, 'Mobile Phone 3', 1, 4, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.', 245, 'uploads/0b1a0e64e4.jpg', 'Black', '2017-09-12'),
-(7, 'Laptop', 8, 2, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.', 850, 'uploads/3b664d7884.jpg', 'Gold', '2017-09-12'),
-(8, 'Noot Book', 8, 5, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.', 280, 'uploads/34cf10e769.jpg', 'Black', '2017-09-12'),
-(9, 'Kid\'s Toy', 8, 2, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.', 80, 'uploads/6aa72cbffa.jpg', 'Pink', '2017-09-12'),
-(10, 'Refrigerator', 7, 6, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.', 820, 'uploads/a5380c6ae6.jpg', 'Any', '2017-09-12'),
-(11, 'Blander', 7, 1, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.', 340, 'uploads/f51b9cf2df.jpg', 'Black', '2017-09-13'),
-(12, 'Washing Machine', 7, 3, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.', 518, 'uploads/38fb57de5f.jpg', 'Blu', '2017-09-12'),
-(13, 'Refrigerator', 6, 6, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.', 820, 'uploads/7853f89919.jpg', 'Blue', '2017-09-13'),
-(14, 'LED Tv', 6, 1, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.', 680, 'uploads/b15bb1ed5d.jpg', 'Black', '2017-09-13'),
-(15, 'Washing Matchine', 6, 3, 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.', 515, 'uploads/696839565c.jpg', 'Black', '2017-09-13');
+(1, 'Speakers', 2, 2, 'This is a test basis texts , you can edit these from admin panel of the side. got to admin/login.php to edit .', 320, 'uploads/1cbd8190ab.jpg', 'Black', '2017-09-12'),
+(2, 'Headphones', 2, 1, 'This is a test basis texts , you can edit these from admin panel of the side. got to admin/login.php to edit .', 150, 'uploads/8c1bbc01ca.jpg', 'Sliver', '2017-09-12'),
+(3, 'Audio Player', 2, 3, 'This is a test basis texts , you can edit these from admin panel of the side. got to admin/login.php to edit .', 180, 'uploads/1911fee744.jpg', 'Blue', '2017-09-12'),
+(4, 'Mobile Phone1', 1, 1, 'This is a test basis texts , you can edit these from admin panel of the side. got to admin/login.php to edit .', 350, 'uploads/25bdaf15a9.jpg', 'Black', '2017-09-12'),
+(5, 'Mobile Phone 2', 1, 2, 'This is a test basis texts , you can edit these from admin panel of the side. got to admin/login.php to edit .', 302, 'uploads/5829e9c083.jpg', 'Sliver', '2017-09-12'),
+(6, 'Mobile Phone 3', 1, 4, 'This is a test basis texts , you can edit these from admin panel of the side. got to admin/login.php to edit .', 245, 'uploads/0b1a0e64e4.jpg', 'Black', '2017-09-12'),
+(7, 'Laptop', 8, 2, 'This is a test basis texts , you can edit these from admin panel of the side. got to admin/login.php to edit .', 850, 'uploads/3b664d7884.jpg', 'Gold', '2017-09-12'),
+(8, 'Noot Book', 8, 5, 'This is a test basis texts , you can edit these from admin panel of the side. got to admin/login.php to edit .', 280, 'uploads/34cf10e769.jpg', 'Black', '2017-09-12'),
+(9, 'Kid\'s Toy', 8, 2, 'This is a test basis texts , you can edit these from admin panel of the side. got to admin/login.php to edit .', 80, 'uploads/6aa72cbffa.jpg', 'Pink', '2017-09-12'),
+(10, 'Refrigerator', 7, 6, 'This is a test basis texts , you can edit these from admin panel of the side. got to admin/login.php to edit .', 820, 'uploads/a5380c6ae6.jpg', 'Any', '2017-09-12'),
+(11, 'Blander', 7, 1, 'This is a test basis texts , you can edit these from admin panel of the side. got to admin/login.php to edit .', 340, 'uploads/f51b9cf2df.jpg', 'Black', '2017-09-13'),
+(12, 'Washing Machine', 7, 3, 'This is a test basis texts , you can edit these from admin panel of the side. got to admin/login.php to edit .', 518, 'uploads/38fb57de5f.jpg', 'Blu', '2017-09-12'),
+(13, 'Refrigerator', 6, 6, 'This is a test basis texts , you can edit these from admin panel of the side. got to admin/login.php to edit .', 820, 'uploads/7853f89919.jpg', 'Blue', '2017-09-13'),
+(14, 'LED Tv', 6, 1, 'This is a test basis texts , you can edit these from admin panel of the side. got to admin/login.php to edit .', 680, 'uploads/b15bb1ed5d.jpg', 'Black', '2017-09-13'),
+(15, 'Washing Matchine', 6, 3, 'This is a test basis texts , you can edit these from admin panel of the side. got to admin/login.php to edit .', 515, 'uploads/696839565c.jpg', 'Black', '2017-09-13');
 
 -- --------------------------------------------------------
 
@@ -268,8 +270,8 @@ CREATE TABLE `ecom_product_review` (
 --
 
 INSERT INTO `ecom_product_review` (`revid`, `cmrid`, `proid`, `name`, `email`, `phone`, `review`, `rate`, `date`) VALUES
-(1, 1, 10, 'Tushar Khan', 'tushar@gmail.com', '+8801962837564', 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.', 3, '2017-09-14'),
-(2, 1, 4, 'Tushar Khan', 'tushar.khan0122@gmail.com', '+8801962837564', 'Test Rewiew', 3, '2017-09-27');
+(1, 1, 10, 'Tanjil Hasan', 'tanjilh136@gmail.com', '+880111111111', ':P this ', 3, '2020-04-16'),
+(2, 1, 4, 'Tanjil Hasan', 'tanjilh136@gmail.com', '+88012222222222', 'Test Rewiew', 3, '2020-04-08');
 
 -- --------------------------------------------------------
 
@@ -370,57 +372,68 @@ ALTER TABLE `ecom_social_media`
 -- AUTO_INCREMENT for table `ecom_admin`
 --
 ALTER TABLE `ecom_admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `ecom_admin_message`
 --
 ALTER TABLE `ecom_admin_message`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `ecom_brand`
 --
 ALTER TABLE `ecom_brand`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT for table `ecom_cart`
 --
 ALTER TABLE `ecom_cart`
   MODIFY `cartid` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `ecom_category`
 --
 ALTER TABLE `ecom_category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
 -- AUTO_INCREMENT for table `ecom_customer`
 --
 ALTER TABLE `ecom_customer`
-  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `ecom_customer_message`
 --
 ALTER TABLE `ecom_customer_message`
   MODIFY `messageid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `ecom_customer_order`
 --
 ALTER TABLE `ecom_customer_order`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
 -- AUTO_INCREMENT for table `ecom_product`
 --
 ALTER TABLE `ecom_product`
   MODIFY `proid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
 --
 -- AUTO_INCREMENT for table `ecom_product_review`
 --
 ALTER TABLE `ecom_product_review`
   MODIFY `revid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `ecom_social_media`
 --
 ALTER TABLE `ecom_social_media`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
