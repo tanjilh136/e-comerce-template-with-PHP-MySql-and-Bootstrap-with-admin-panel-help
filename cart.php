1a654fe4c99352fccfaacbe783d51773df9012cf
Tanjil Hasan <41756334+tanjilh136@users.noreply.github.com>
2020-09-11 18:53:26 +0600

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Tushar Khan
+ * User: Tanjil Hasan
  * Date: 9/15/2017
  * Time: 2:17 AM
  */
@@ -29,7 +29,7 @@
         <!-- for-mobile-apps -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="author" content="Tushar Khan" />
+        <meta name="author" content="Tanjil Hasan" />
         <script type="application/x-javascript">
             addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
             function hideURLbar(){ window.scrollTo(0,1); }
